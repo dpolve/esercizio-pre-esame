fb21ca6f25c2dc800e2c0c2b3df322010b30ad5b
adm-polvedav <dawde.din@gmail.com>
2020-10-07 17:00:48 +0200

    loggin ok

--- DAVIDE/argomenti.php
+++ DAVIDE/argomenti.php
@@ -1,13 +1,10 @@
+<?php include("./conf.php") ?>
 <html>
 
 <head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
-
     <title>Elenco Argomenti</title>
 </head>
 
@@ -20,7 +17,6 @@
         </div>
         <h1>Quale argomento ti interessa?</h1>
         <?php
-        include("./conf.php");
         // 2. lettura records di tabella argomenti
         $argomenti = " SELECT arg_id,arg_argomento FROM argomenti ORDER BY arg_argomento ASC ";
         $st_argomenti = $conn->prepare($argomenti);
@@ -48,14 +44,6 @@
                 ?>
             </tbody>
         </table>
-
-
-
-        <!-- Optional JavaScript -->
-        <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
     </div>
 </body>
 
