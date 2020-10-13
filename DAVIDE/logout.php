a7e230ee83c78166e9edf07df75097bd6f0c30d5
Lorenzo <tamburini.lorenzo@libero.it>
2020-10-13 16:12:12 +0200

    merge conflict

---
+++ DAVIDE/logout.php
@@ -0,0 +1,15 @@
+<?php
+    //documento php richiesto per il logout
+
+    //ripristino della sezione
+    session_start();
+
+    //libera tutte le variabili registrate in sessione
+    session_unset();
+
+    //distrugge tutti i dati assciati alla sessione
+    session_destroy();
+    
+    //mi sposto nell'index.php indicando come testo "LogOut completato"
+    header("Location: index.php?msg=Logout completato");
+?>
--- DAVIDE/menu.php
+++ DAVIDE/menu.php
@@ -1,6 +1,7 @@
+<html>
+<!-- inizio html pagina riservata -->
 <?php
-session_start();
-
+include("./conf.php");
 if (!$_SESSION['login']) {
     header("Location: index.php?msg=Autenticazione necessaria");
 } else if ($_SESSION['ute_ruolo'] == "users") {
@@ -8,10 +9,6 @@ if (!$_SESSION['login']) {
 } else {
     echo "Area riservata ADMIN";
 ?>
-
-    <!-- inizio html pagina riservata -->
-    <?php include("./conf.php") ?>
-    <html>
 
     <head>
         <!-- Required meta tags -->
@@ -27,23 +24,39 @@ if (!$_SESSION['login']) {
                 <button type="button" class="btn btn-secondary">Gestione Utenti</button>
                 <a href="#" class="btn btn-primary btn-outline-danger">Esci (LogOut)</a>
             </div>
-            <h1>Statistiche <a href="menu.php?freq=T"> Totali</a>,<a href="menu.php?freq=I"> Ieri</a>,<a href="menu.php?freq=O"> Oggi </a>per Argomento</h1>
+            <h1>
+                Statistiche per <a href="menu.php?freq=T&argeve=A"> Argomenti </a>
+                o <a href="menu.php?freq=T&argeve=E"> Eventi </a>
+            </h1>
+            <br>
+            <a href="menu.php?freq=T&argeve=A"> Totali</a>
+            ,<a href="menu.php?freq=I"> Ieri</a>
+            ,<a href="menu.php?freq=O"> Oggi </a>
             <?php
             $today = date("Y-m-d");
             $day_before = date("Y-m-d", strtotime($today . ' -1 day'));
 
 
             $freq = $_REQUEST["freq"];
+            $argeve = $_REQUEST["argeve"];
+            if ($_REQUEST["argeve"] == "A") {
+                echo "carica argomenti";
+                exit;
+            } else {
+                echo "carica eventi";
+                exit;
+            }
+            die();
             if ($_REQUEST["freq"] == "T") {
                 // 2. lettura records di tabella statistiche totali per argomento
                 $stat_sum =
                     " SELECT argomenti.arg_argomento , argomenti.arg_id ,
-                        COUNT(argomenti.arg_argomento) as sta_tot 
-                        FROM statistiche
-                        INNER JOIN eventi ON eventi.eve_id = statistiche.sta_eve_id
-                        INNER JOIN argomenti ON eventi.eve_arg_id = argomenti.arg_id
-                        GROUP BY argomenti.arg_argomento
-                        ORDER BY argomenti.arg_argomento  ";
+                    COUNT(argomenti.arg_argomento) as sta_tot 
+                    FROM statistiche
+                    INNER JOIN eventi ON eventi.eve_id = statistiche.sta_eve_id
+                    INNER JOIN argomenti ON eventi.eve_arg_id = argomenti.arg_id
+                    GROUP BY argomenti.arg_argomento
+                    ORDER BY argomenti.arg_argomento  ";
                 $st_stat_sum = $conn->prepare($stat_sum);
                 $st_stat_sum->execute();
                 $sta_sum = $st_stat_sum->fetchAll(PDO::FETCH_ASSOC);
@@ -65,14 +78,14 @@ if (!$_SESSION['login']) {
                             $arg_a = $record["arg_argomento"];
                             $sta_tot = $record["sta_tot"];
                             echo '
-                        <tr>
-                           <td>
-                                <a href="' . $link_id . $arg_id . $link_argomento . $arg_a . '">' . $arg_a . '</a>
-                            </td>
-                            <td>
-                            ' . $sta_tot . '
-                            </td>
-                        </tr>';
+            <tr>
+                <td>
+                    <a href="' . $link_id . $arg_id . $link_argomento . $arg_a . '">' . $arg_a . '</a>
+                </td>
+                <td>
+                ' . $sta_tot . '
+                </td>
+            </tr>';
                         }
                         ?>
                     </tbody>
@@ -80,19 +93,20 @@ if (!$_SESSION['login']) {
             <?php
                 exit;
             } else if ($_REQUEST["freq"] == "I") {
-                // 2.1 lettura records di tabella statistiche totali per argomento
+                // 2.1 lettura records di tabella statistiche Ieri per argomento
                 $stat_ier =
                     " SELECT argomenti.arg_id , argomenti.arg_argomento , COUNT(*) as sta_tot
-                    FROM statistiche 
-                    INNER JOIN eventi on eventi.eve_id = statistiche.sta_eve_id
-                    INNER JOIN argomenti ON eventi.eve_arg_id = argomenti.arg_id
-                    WHERE statistiche.sta_data = '$day_before'
-                    GROUP BY argomenti.arg_argomento ";
+        FROM statistiche 
+        INNER JOIN eventi on eventi.eve_id = statistiche.sta_eve_id
+        INNER JOIN argomenti ON eventi.eve_arg_id = argomenti.arg_id
+        WHERE statistiche.sta_data = '$day_before'
+        GROUP BY argomenti.arg_argomento ";
                 $st_stat_ier = $conn->prepare($stat_ier);
                 $st_stat_ier->execute();
                 $sta_ier = $st_stat_ier->fetchAll(PDO::FETCH_ASSOC);
 
-            ?><!-- <pre><?php var_dump($sta_ier)?></pre> -->
+            ?>
+                <!-- <pre><?php var_dump($sta_ier) ?></pre> -->
                 <table class="table table-striped">
                     <thead>
                         <tr>
@@ -110,8 +124,54 @@ if (!$_SESSION['login']) {
                             $arg_a = $record["arg_argomento"];
                             $sta_tot = $record["sta_tot"];
                             echo '
+                <tr>
+                    <td>
+                        <a href="' . $link_id . $arg_id . $link_argomento . $arg_a . '">' . $arg_a . '</a>
+                    </td>
+                    <td>
+                    ' . $sta_tot . '
+                    </td>
+                </tr>';
+                        }
+                        ?>
+                    </tbody>
+                </table>
+            <?php
+                exit;
+            } else {
+                // 2.1 lettura records di tabella statistiche OGGI per argomento
+                $stat_ogg =
+                    " SELECT argomenti.arg_id , argomenti.arg_argomento , COUNT(*) as sta_tot
+        FROM statistiche 
+        INNER JOIN eventi on eventi.eve_id = statistiche.sta_eve_id
+        INNER JOIN argomenti ON eventi.eve_arg_id = argomenti.arg_id
+        WHERE statistiche.sta_data = '$today'
+        GROUP BY argomenti.arg_argomento ";
+                $st_stat_ogg = $conn->prepare($stat_ogg);
+                $st_stat_ogg->execute();
+                $sta_ogg = $st_stat_ogg->fetchAll(PDO::FETCH_ASSOC);
+
+            ?>
+                <!-- <pre><?php var_dump($sta_ogg) ?></pre> -->
+                <table class="table table-striped">
+                    <thead>
+                        <tr>
+                            <th scope="col">Statistiche di Argomenti</th>
+                            <th scope="col">Visite di Ieri: <?php echo $day_before ?></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                        $link_id = "stat_dettaglio.php?id_argomento=";
+                        $link_argomento = "&argomento_nome=";
+
+                        foreach ($sta_ogg as $record) {
+                            $arg_id = $record["arg_id"];
+                            $arg_a = $record["arg_argomento"];
+                            $sta_tot = $record["sta_tot"];
+                            echo '
                             <tr>
-                               <td>
+                                <td>
                                     <a href="' . $link_id . $arg_id . $link_argomento . $arg_a . '">' . $arg_a . '</a>
                                 </td>
                                 <td>
@@ -122,47 +182,12 @@ if (!$_SESSION['login']) {
                         ?>
                     </tbody>
                 </table>
-            <?php
-                exit;
-            } else {
-                echo "Oggi";
+        <?php
                 exit;
             }
-            ?>
-            <table class="table table-striped">
-                <thead>
-                    <tr>
-                        <th scope="col">Statistiche di Argomenti</th>
-                        <th scope="col">Visite totali</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <?php
-                    $link_id = "stat_dettaglio.php?id_argomento=";
-                    $link_argomento = "&argomento_nome=";
-
-                    foreach ($sta_sum as $record) {
-                        $arg_id = $record["arg_id"];
-                        $arg_a = $record["arg_argomento"];
-                        $sta_tot = $record["sta_tot"];
-                        echo '
-                        <tr>
-                           <td>
-                                <a href="' . $link_id . $arg_id . $link_argomento . $arg_a . '">' . $arg_a . '</a>
-                            </td>
-                            <td>
-                            ' . $sta_tot . '
-                            </td>
-                        </tr>';
-                    }
-                    ?>
-                </tbody>
-            </table>
+        }
+        ?>
         </div>
     </body>
 
-    </html>
-
-<?php
-}
-?>
+</html>
