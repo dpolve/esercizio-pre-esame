ce94580a2186bc56b29115050d242d85e2153a0d
Lorenzo <tamburini.lorenzo@libero.it>
2020-10-13 16:15:03 +0200

    problemi risolti

--- LORENZO/index.php
+++ LORENZO/index.php
@@ -21,7 +21,6 @@
         <div class="row">
             <div class="col-md-6 offset-md-4">
                 <form method="POST" action="check-login.php">
-<<<<<<< HEAD
                     <div class="form-group row">
                         <label for="utenteLogin" class="col-sm-2">Nome Utente</label>
                         <input name="utenteLogin" type="text" class="form-control col-sm-4" id="utenteLogin">
@@ -29,12 +28,6 @@
                     <div class="form-group row">
                         <label for="passwordLogin" class="col-sm-2">Password</label>
                         <input name="passwordLogin" type="password" class="form-control col-sm-4" id="passwordLogin">
-=======
-                    <div class="form-group">
-                        <br>
-                        <label for="utenteLogin">Nome Utente</label>
-                        <input name="utenteLogin" type="text" class="form-control" id="utenteLogin">
->>>>>>> c2e9fcb4c58797635f0f7192e36b38a268e59245
                     </div>
                     <div class="form-group row">
                         <button type="submit" class="btn btn-primary col-sm-2 offset-md-3">Submit</button>
